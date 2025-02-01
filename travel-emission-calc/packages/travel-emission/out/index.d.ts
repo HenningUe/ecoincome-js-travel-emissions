804bd2f75b447040cadb373812b93fa8db19a75a
Henning Uekötter <121238438+HenningUe@users.noreply.github.com>
2025-02-01 14:06:33 +0000

    node_muodules/ removed

--- travel-emission-calc/packages/travel-emission/out/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { TransportationMode } from './enums';
-export declare function getDistanceKm(apiKey: string, transportMode: TransportationMode | string, origin: string, destination: string): Promise<number>;
-export declare function getCO2EmissionKgTotalPerPerson(apiKey: string, transportMode: TransportationMode | string, origin: string, destination: string): Promise<number>;
