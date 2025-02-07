99ea18ed2005d775626aecd9997048bb0aba88d2
Henning Uekötter <121238438+HenningUe@users.noreply.github.com>
2025-02-07 16:21:05 +0000

    readme filled in

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # ecoincome-js-travel-emissions
-Calculator of business travel emissions
+Example implementation of a REST API interface for recording and evaluating CO2 emissions caused by (business) travel.
+
+Used technology:
+- typescript
+- nestjs
+- typeorm
+- postgres
+- github-codespace
+
+The example is executable in the codespace of this repo.
+
+The server runs locally on port 3000. Requests can be sent via a corresponding client (e.g. browser).
+
+Functions implemented:
+- getCO2EmissionKgTotalPerPerson
+  Calculates CO2 emissions for trips from the starting point to the destination. The distance is determined automatically. Depending on the method of transportation, different CO2/km emissions are assumed
+- addTravelRecord
+  Add travel record. An entry includes company name, starting point, destination, means of transportation and travel date
+- getCO2EmissionKgPerDateRange
+  Calculates CO2 emissions for trips from the origin to the destination for a given date range, company and transportation mode.
+  The distance is determined automatically. Depending on the means of transportation, different CO2/km emissions are assumed. 
--- travel-emission-api/src/app.controller.ts
+++ travel-emission-api/src/app.controller.ts
@@ -9,6 +9,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('getCO2EmissionKgTotalPerPerson')
+  /**
+   * Calculates CO2 emissions for trips from the origin to the destination per person
+   * The distance is determined automatically. Depending on the means of transportation, 
+   * different CO2/km emissions are assumed.
+   * @param paramDto getCO2EmissionSinglePersonDto The object containing the parameters
+   * @returns A string with the total CO2 emission in kg
+   */
   async getCO2EmissionKgTotalPerPerson(
     @Query(new ValidationPipe({ transform: true })) paramDto: getCO2EmissionSinglePersonDto,
   ): Promise<string> {
@@ -18,6 +25,14 @@ export class AppController {
 
 
   @Get('getCO2EmissionKgPerDateRange')
+  /**
+   * Calculates CO2 emissions for trips from the origin to the destination
+   * for a given date range, company and transportation mode.
+   * The distance is determined automatically. Depending on the means of transportation, 
+   * different CO2/km emissions are assumed.
+   * @param paramDto getCO2EmissionPerDateRangeDto The object containing the parameters
+   * @returns A string with the total CO2 emission in kg
+   */
   async getCO2EmissionKgPerDateRange(
     @Query(new ValidationPipe({ transform: true })) paramDto: getCO2EmissionPerDateRangeDto,
   ): Promise<string> {
@@ -26,6 +41,12 @@ export class AppController {
   }
 
   @Get('addTravelRecordAsGet')
+  /**
+   * Add travel record. An entry includes company name, starting point, destination, means of transportation and travel date.
+   * This method is called as a GET request, so the parameters are passed in the URL.
+   * @param paramDto addTravelRecordDto The object containing the parameters
+   * @returns A string with the result of the operation
+   */
   async addTravelRecordAsGet(
     @Query(new ValidationPipe({ transform: true })) paramDto: addTravelRecordDto,
   ): Promise<string> {
@@ -35,6 +56,11 @@ export class AppController {
 
 
   @Post('addTravelRecord')
+  /**
+   * Add travel record. An entry includes company name, starting point, destination, means of transportation and travel date.
+   * @param paramDto addTravelRecordDto The object containing the parameters
+   * @returns A string with the result of the operation
+   */
   async addTravelRecord(
     @Body(new ValidationPipe({ transform: true })) paramDto: addTravelRecordDto,
   ): Promise<string> {
