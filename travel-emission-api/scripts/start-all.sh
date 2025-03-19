6a630e26fd07a33118794e53c8ed834b1cbf07c6
Henning Uekötter <121238438+HenningUe@users.noreply.github.com>
2025-03-19 16:05:48 +0000

    tagging added

--- travel-emission-api/scripts/start-all.sh
+++ travel-emission-api/scripts/start-all.sh
@@ -3,6 +3,8 @@
 MODE=DEV
 PRIVATENETWORKNAME=PRIVATENETWORKNAME
 POSTGRES_HOST_PRNET=172.28.1.1
+docker kill $(docker images --filter=reference="*postgres*" -q)
+docker kill $(docker images --filter=reference="*ecoincome-travel-emission-api*" -q)
 docker network rm $PRIVATENETWORKNAME
 docker network create --subnet=172.28.0.0/16  $PRIVATENETWORKNAME
 source start-db.sh
